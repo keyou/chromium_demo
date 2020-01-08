aee4d11f5cbed125a4df09729360a92d49d0a23b
keyou <keyou@cvte.com>
2020-01-08 16:53:43 +0800

    update: demo_log

--- demo_log.cc
+++ demo_log.cc
@@ -4,9 +4,12 @@
 int main(int argc, char** argv) {
   base::CommandLine::Init(argc, argv);
 
+  int loglevel = -1;
+
   logging::SetLogPrefix("demo");
   logging::SetLogItems(true,false,true,false);
-  logging::SetMinLogLevel(-1);
+  // 设置为负数启用VLOG
+  logging::SetMinLogLevel(loglevel);
 
   logging::LoggingSettings settings;
   settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG;
@@ -16,9 +19,31 @@ int main(int argc, char** argv) {
   bool logging_res = logging::InitLogging(settings);
   CHECK(logging_res);
 
-  LOG(INFO) << "hello,demo!";
-  VLOG(2) << "hello,vlog 2";
-  VLOG(1) << "hello,vlog 1";
+  LOG(INFO) << "set loglevel to: " << loglevel;
+
+  LOG(INFO) << "LOG: INFO";
+  DLOG(INFO) << "DLOG: INFO";
+
+  LOG(WARNING) << "LOG: WARNING";
+  LOG(ERROR) << "LOG: ERROR";
+
+  LOG_IF(INFO,2>1) << "LOG_IF: INFO 2>1";
+  DLOG_IF(INFO,2>1) << "DLOG_IF: INFO 2>1";
+
+  // 不会打印出来，因为我们设置了LogLevel为 -1
+  VLOG(2) << "VLOG: 2";
+  VLOG(3) << "VLOG: 3";
+
+  VLOG(1) << "VLOG: 1";
+  DVLOG(1) << "DVLOG: 1";
+
+  // 在日志后面追加 GetLastError() on Windows and errno on POSIX
+  PLOG(INFO) << "PLOG: INFO";
+  DPLOG(INFO) << "DPLOG: INFO";
+
+  // 会使进程立即退出
+  DLOG(FATAL) << "DLOG: FATAL";
+  LOG(FATAL) << "LOG: FATAL";
 
   return 0;
 }
