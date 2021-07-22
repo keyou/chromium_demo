ef1cf4c4ebdb5631b56ca200bbb412e0ba0017af
keyou <keyou@users.noreply.github.com>
2021-07-22 13:54:07 +0800

    demo_exe: add more comments

--- demo.cc
+++ demo.cc
@@ -1,3 +1,12 @@
+/**
+ * 迁移说明：
+ * -------
+ * 80 -> 91:
+ * - base::MessageLoop 被移除，使用 base::SingleThreadTaskExecutor 代替，详见
+ *   https://bugs.chromium.org/p/chromium/issues/detail?id=891670&q=891670&can=2
+ * 
+ */
+
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/logging.h"
@@ -5,6 +14,10 @@
 #include "base/task/single_thread_task_executor.h"
 #include "base/task/thread_pool/thread_pool_instance.h"
 
+/**
+ * 这个项目是个基础的入门demo，关于消息循环如果现在不明白可以先不用关注它，后面的
+ * demo_task_executor 会专门演示它。
+ */
 int main(int argc, char** argv) {
   // 类似C++的 atexit() 方法，用于管理程序的销毁逻辑，base::Singleton类依赖它
   base::AtExitManager at_exit;
